384722625928e01fd6dbe180e9da81d65e7cdb3d
John McNair <john.mcnair@yellowdog.co>
2026-03-28 15:22:28 +0000

    Add infrastructure configuration

---
+++ infra/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-cloudflare/sdk/v5/go/cloudflare"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
+)
+
+func main() {
+	pulumi.Run(func(ctx *pulumi.Context) error {
+		cfg := config.New(ctx, "")
+		accountID := cfg.Require("accountId")
+
+		bucket, err := cloudflare.NewR2Bucket(ctx, "solar-data", &cloudflare.R2BucketArgs{
+			AccountId: pulumi.String(accountID),
+			Name:      pulumi.String("solar-data"),
+			Location:  pulumi.String("WEUR"), // Western Europe — closest to UK
+		})
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("bucketName", bucket.Name)
+
+		return nil
+	})
+}
